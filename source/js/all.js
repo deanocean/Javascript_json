7c9bb265ca380eb4320261511d27b34d446e57a9
Dean <deanocean5656@gmail.com>
2020-08-19 17:29:56 +0900

    js restructure

--- source/js/all.js
+++ source/js/all.js
@@ -1,10 +1,27 @@
-
+// init
 let data;
+let pageArea = "全部列表";
 let cardPerPage = 10;
 
-init()
+dataLoad()
+renderAreaList( filterAreaList() );
+renderArea(data);
+renderPage(data);
 
-function loadData(){
+$('.hot .btn').click(function(e){ 
+    renderArea( filterArea(e, $(this)) ); 
+    renderPage( filterArea(e, $(this)) );
+})
+
+$('.pagination').click(function(e){ clickPage(e, $(this)) })
+
+$('header .dropdown-menu').click( function(e) { 
+    renderArea( filterArea(e, $(this)) ); 
+    renderPage( filterArea(e, $(this)) );
+} )
+
+
+function dataLoad(){
     let url = "https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json";
     let xhr = new XMLHttpRequest();
     xhr.open("GET", url, false);
@@ -13,23 +30,14 @@ function loadData(){
     if(xhr.status == 200){
         let back = JSON.parse(xhr.responseText);
         data = back.result.records;
-        // console.log(data);
-        showData(data);
-        zoneList();
     }
 }
 
-function zoneList(){
-    let arr = filterZoneList();
-    showZoneList(arr);
-}
-
-function filterZoneList(){ 
-    // 從json資料中的data[i].Zone，取出所有的行政區
+function filterAreaList(){ 
+    // 從json資料中的data[i].Area，取出所有的行政區
     // 加進陣列的過程要跳過重複的區域
     let content = [];
     for(var i = 0; i < data.length; i++){
-        // console.log(data[i].Zone)
         let bool = content.find(function(el){ 
             // array.find() 回傳第一個滿足條件的值，如果找不到則回傳 undefined。
             // 如果陣列裡有該區域的話，就回傳該區域的值，找不到就會回傳undefined。
@@ -39,49 +47,46 @@ function filterZoneList(){
             content.push(data[i].Zone);
         }
     }
-    // console.log(content);
     return content;
 }
 
-function showZoneList(arr){
+function renderAreaList(filteredData){
+    let arr = filteredData;
     let str = "";
     for(let i = 0; i < arr.length; i++){
-        str += `<a class="dropdown-item" href="#">${arr[i]}</a>`;
+        str += `<a class="dropdown-item" data-area="${arr[i]}" href="#">${arr[i]}</a>`;
     }
     $('header .dropdown-menu').html(str);
-    $('header .dropdown-item').click( function(e) { judgeBtnContent(e, $(this)) } )
 }
 
-function judgeBtnContent(e, obj){
-    e.preventDefault();
-    let strict = obj.text();
-    let filteredData = filterZone(strict);
-    console.log(filteredData)
-    $('.infoArea h2').text(strict);
-    $('.infoArea h2').attr('data-strict',strict);
-    showData(filteredData);
-    showPage(filteredData);
-}
-
-function filterZone(strict){
+function filterArea(e = null, obj = null){
     let filteredData = [];
 
-    if(strict == "all"){
+    if(e != null){
+        e.preventDefault();
+        pageArea = $(e.target).data('area');
+    }
+
+    if(pageArea == "全部列表"){
         return data;
     }
 
     for(let i = 0; i < data.length; i++){
-        if(data[i].Zone == strict){
+        if(data[i].Zone == pageArea){
             filteredData.push(data[i]);
         }
     }
     return filteredData;
 }
 
-function showData(filteredData, index = 0){
-    // console.log("index", index);
+function renderArea(filteredData, index = 0){
     let content = '';
+
     let dataPerPage = filteredData.slice(index, index+cardPerPage);
+
+    $('.infoArea h2').text(pageArea);
+    $('.infoArea h2').attr('data-area', pageArea);
+
     for(var i = 0; i < dataPerPage.length; i++){
         let template = `
         <div class="col mb-4">
@@ -104,20 +109,18 @@ function showData(filteredData, index = 0){
 }
 
 function clickPage(e, obj){
-    console.log(e.target)
     e.preventDefault()
     let index = ( parseInt(e.target.textContent) - 1 ) * cardPerPage;  // 1: 0 | 2: 6 送給showData()的slice處理，它會從0開始抓0~5、從6開始抓6~11
-    let strict = $('.infoArea h2').data('strict');
-    console.log("strict", strict);
-    let filteredData = filterZone(strict);
-    
-    showData(filteredData, index);
+    let area = $('.infoArea h2').data('area');
+    let filteredData = filterArea();
+    console.log(filteredData);
+    renderArea(filteredData, index);
     
     obj.find(".page-item").removeClass('active');
     $(e.target).parent().addClass('active');
 }
 
-function showPage (filteredData){
+function renderPage (filteredData){
     let str = '';
     let length = filteredData.length;
     let pageAmt = Math.ceil((length / cardPerPage));
@@ -130,12 +133,54 @@ function showPage (filteredData){
     $('.pagination .page-item').eq(0).addClass("active");
 }
 
-function init(){
-    loadData()
-    showPage(data)
-    $('.hot .btn').click(function(e){ judgeBtnContent(e, $(this)) })
-    $('.pagination').click(function(e){ clickPage(e, $(this)) })
-}
-
 
 // 順一下流程
+    // 資料
+    // 事件
+    // 渲染
+
+// 功能
+/*
+    資料 撈取全部內容 json
+    
+    註冊 下拉選單 點擊 (顯示列表資料) 
+
+    資料 篩選 下拉選單內顯示的區域 
+
+    繪製 下拉選單 在header   
+
+    註冊 熱門行政區 點擊 (顯示列表資料)
+
+    資料 篩選 被點擊的熱門區域所屬內容
+
+    繪製 列表資料 在內容
+
+    註冊 pagination 點擊 (顯示出對應頁數的內容)
+
+    資料 篩選 pagination的頁數
+
+    繪製 pagination 在內容下方
+*/
+
+// function
+/* 
+    dataLoad()
+
+    filterAreaList()
+
+    renderAreaList()
+    
+    filterArea()
+
+    renderArea()
+
+    filterPage()
+
+    renderPage()
+
+*/
+
+// init
+/*
+
+*/
